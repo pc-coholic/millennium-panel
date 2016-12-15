31d65dee42dadd726452fdf0275c73f674142910
Martin Gross <martin.gross@rtt.ag>
2016-12-15 17:16:54 +0100

    Adding NCC Termial access parameters

--- src/millennium/panel/admin.py
+++ src/millennium/panel/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from millennium.panel.models import NCCTermParms
 # Register your models here.
 
 admin.site.site_title = 'Millennium Panel'
 admin.site.site_header = 'Millennium Panel'
 admin.site.index_title = 'Administration'
+
+class NCCTermParmsAdmin(admin.ModelAdmin):
+    exclude = ('tenant',)
+    list_display = ('name', 'primaryNCCnumber', 'alternateNCCnumber', 'CADId', 'CPEId')
+
+    def get_queryset(self, request):
+        return NCCTermParms.objects.filter(tenant=request.session['tenant'])
+
+    def has_change_permission(self, request, obj=None):
+        has_class_permission = super(NCCTermParmsAdmin, self).has_change_permission(request, obj)
+        if not has_class_permission:
+            return False
+        if obj is not None and obj.tenant != int(request.session['tenant']) and request.user.groups.filter(id=obj.tenant.id).exists() != True:
+           return False
+        return True
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.tenant = Group.objects.get(id=request.session['tenant'])
+        obj.save()
+
+admin.site.register(NCCTermParms, NCCTermParmsAdmin)
--- src/millennium/panel/models.py
+++ src/millennium/panel/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
 # Create your models here.
+
+class NCCTermParms(models.Model):
+    name = models.CharField(max_length=32)
+    tenant = models.ForeignKey(Group, on_delete=models.CASCADE)
+    primaryNCCnumber = models.DecimalField(max_digits=12, decimal_places=0, verbose_name='Primary NCC number')
+    alternateNCCnumber = models.DecimalField(max_digits=12, decimal_places=0, verbose_name='Alternate NCC number')
+    CADId = models.DecimalField(max_digits=8, decimal_places=0, verbose_name='CAD ID', blank=True, null=True)
+    CPEId = models.DecimalField(max_digits=8, decimal_places=0, verbose_name='CPE ID', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        unique_together = (('name', 'tenant'),)
+        verbose_name = 'NCC/terminal access parameters'
+        verbose_name_plural = 'NCC/terminal access parameters'
+
