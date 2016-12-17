78a2755c2ecc0389f1567156d26d7a24108be98a
Martin Gross <martin@pc-coholic.de>
2016-12-17 23:24:25 +0100

    Adding InstallParms and FconfigOpts models and Admin

--- src/millennium/panel/admin.py
+++ src/millennium/panel/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from millennium.panel.models import NCCTermParms
+from millennium.panel.models import *
 # Register your models here.
 
 admin.site.site_title = 'Millennium Panel'
@@ -9,7 +9,7 @@ admin.site.index_title = 'Administration'
 
 class NCCTermParmsAdmin(admin.ModelAdmin):
     exclude = ('tenant',)
-    list_display = ('name', 'primaryNCCnumber', 'alternateNCCnumber', 'CADId', 'CPEId')
+    list_display = ('name', 'datapac_num', 'alt_datapac_num', 'cad_id', 'cpe_id')
 
     def get_queryset(self, request):
         return NCCTermParms.objects.filter(tenant=request.session['tenant'])
@@ -27,4 +27,47 @@ class NCCTermParmsAdmin(admin.ModelAdmin):
             obj.tenant = Group.objects.get(id=request.session['tenant'])
         obj.save()
 
+class InstallParmsAdmin(admin.ModelAdmin):
+    exclude = ('tenant',)
+    list_display = ('name',)
+
+    def get_queryset(self, request):
+        return InstallParms.objects.filter(tenant=request.session['tenant'])
+
+    def has_change_permission(self, request, obj=None):
+        has_class_permission = super(InstallParmsAdmin, self).has_change_permission(request, obj)
+        if not has_class_permission:
+            return False
+        if obj is not None and obj.tenant != int(request.session['tenant']) and request.user.groups.filter(id=obj.tenant.id).exists() != True:
+           return False
+        return True
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.tenant = Group.objects.get(id=request.session['tenant'])
+        obj.save()
+
+class FconfigOptsAdmin(admin.ModelAdmin):
+    exclude = ('tenant',)
+    list_display = ('name',)
+
+    def get_queryset(self, request):
+        return FconfigOpts.objects.filter(tenant=request.session['tenant'])
+
+    def has_change_permission(self, request, obj=None):
+        has_class_permission = super(FconfigOptsAdmin, self).has_change_permission(request, obj)
+        if not has_class_permission:
+            return False
+        if obj is not None and obj.tenant != int(request.session['tenant']) and request.user.groups.filter(id=obj.tenant.id).exists() != True:
+           return False
+        return True
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.tenant = Group.objects.get(id=request.session['tenant'])
+        obj.save()
+
+
 admin.site.register(NCCTermParms, NCCTermParmsAdmin)
+admin.site.register(InstallParms, InstallParmsAdmin)
+admin.site.register(FconfigOpts, FconfigOptsAdmin)
